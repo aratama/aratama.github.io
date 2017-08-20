be43c4044f629254fab757a0539a4d8a3f30aadb
aratama <aratama@example.com>
2017-08-20 16:28:08 +0900

    update

--- res/gears.js
+++ res/gears.js
@@ -4,10 +4,10 @@ window.addEventListener("DOMContentLoaded", function(){
 
     function render(){
         const r = window.scrollY / 10;
-        const s = 1 - (window.scrollY / 10000); 
+        const s = 2.0 * (1 - (window.scrollY / 10000)); 
         return h("div.background", [
-            h("img.gear.gear0", { key: 0, src: "/res/gear.svg", style: `opacity: 0.1; position:absolute; top:100px; left: 100px; transform: rotate(${r}deg) scale(${s})` }),
-            h("img.gear.gear0", { key: 0, src: "/res/gear.svg", style: `opacity: 0.1; position:absolute; bottom:-500px; right: -300px; transform: rotate(${-r}deg) scale(${s})` })           
+            h("img.gear.gear0", { key: 0, src: "/res/gear.png", style: `opacity: 1; position:absolute; top:100px; left: 100px; transform: rotate(${r}deg) scale(${s})` }),
+            h("img.gear.gear0", { key: 1, src: "/res/gear.png", style: `opacity: 1; position:absolute; bottom:-100px; right: -100px; transform: rotate(${-r}deg) scale(${s})` })           
         ]);      
     }
 
